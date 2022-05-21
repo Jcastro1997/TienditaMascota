5cf72800539d3b0d54b56d5b07c5b6bfd8a82003
Johan Castro <jcastro@naturalphone.cl>
2022-05-20 22:42:47 -0400

    20-05

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,33 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    modalInfo:false,
+    information:[],
+    paymentWidget: false,
+    paymentItems: [],
+    idCategory:null,
+
   },
   getters: {
+
   },
   mutations: {
+    setIdCategory(state, val){
+      state.idCategory = val
+      console.log("ID",val)
+    },
+    setPaymentItems(state, value) {
+      state.paymentItems.push(value);
+    },
+    setPaymentWidget(state, value) {
+      state.paymentWidget = value;
+    },    
+    setModalInfo(state,val){
+      state.modalInfo = val
+    },
+    setInformation(state,val){
+      state.information = val
+    }
   },
   actions: {
   },
